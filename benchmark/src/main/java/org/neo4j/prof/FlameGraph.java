55a2eb6d07f11bcac6d37ed749843a79259509fd
Paul Horn <phorn@avantgarde-labs.de>
2019-04-04 15:17:34 +0200

    Add FlameGraph profiler to generate flame graphs of JMH benchmarks
    To enable it, run the benchmarks with `-prof org.neo4j.prof.FlameGraph`.
    You have to download the [async-profiler](https://github.com/jvm-profiling-tools/async-profiler) and point to its location with `ASYNC_PROFILER_DIR=/path/to/async-profiler` or by using the `asyncProfiler` option.
    Per default, the output files are saved under /tmp (or wherever java.io.tmp points to) in the org.neo4j/bench subdirectory, grouped by the benchmark class.
    Run with `-prof org.neo4j.prof.FlameGraph:help` to see all possible options.

---
+++ benchmark/src/main/java/org/neo4j/prof/FlameGraph.java
@@ -0,0 +1,323 @@
+package org.neo4j.prof;
+
+import joptsimple.ArgumentAcceptingOptionSpec;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+import org.neo4j.graphalgo.helper.ldbc.LdbcDownloader;
+import org.openjdk.jmh.infra.BenchmarkParams;
+import org.openjdk.jmh.infra.IterationParams;
+import org.openjdk.jmh.profile.ExternalProfiler;
+import org.openjdk.jmh.profile.InternalProfiler;
+import org.openjdk.jmh.profile.ProfilerException;
+import org.openjdk.jmh.results.BenchmarkResult;
+import org.openjdk.jmh.results.IterationResult;
+import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.runner.IterationType;
+import org.openjdk.jmh.util.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.OptionalLong;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public final class FlameGraph implements InternalProfiler, ExternalProfiler {
+
+    private static final String ASYNC_PROFILER_DIR = "ASYNC_PROFILER_DIR";
+    private static final String DEFAULT_EVENT = "cpu";
+
+    private final String event;
+    private final Path asyncProfilerDir;
+    private final boolean threads;
+    private final OptionalLong frameBufferSize;
+    private final OptionalLong interval;
+    private final Collection<Path> generated = new ArrayList<>();
+    private Path outputDir;
+    private boolean started;
+    private int measurementIterationCount;
+
+    public FlameGraph(final String initLine) throws ProfilerException {
+        Path asyncDir = null;
+        String env = System.getenv(ASYNC_PROFILER_DIR);
+        if (env != null) {
+            asyncDir = Paths.get(env).toAbsolutePath();
+        }
+
+        OptionParser parser = new OptionParser();
+
+        OptionSpec<String> outputDir = parser
+                .accepts("dir", "Output directory")
+                .withRequiredArg()
+                .describedAs("directory")
+                .ofType(String.class);
+
+        ArgumentAcceptingOptionSpec<String> asyncProfiler = parser
+                .accepts(
+                        "asyncProfiler",
+                        "Location of https://github.com/jvm-profiling-tools/async-profiler unless procived by $" + ASYNC_PROFILER_DIR)
+                .withRequiredArg()
+                .ofType(String.class)
+                .describedAs("directory");
+        if (asyncDir == null) {
+            asyncProfiler.required();
+        } else {
+            asyncProfiler.defaultsTo(asyncDir.toString());
+        }
+
+        OptionSpec<String> event = parser
+                .accepts("event", "Event to sample: cpu, alloc, lock, cache-misses etc.")
+                .withRequiredArg()
+                .ofType(String.class)
+                .defaultsTo("cpu");
+        OptionSpec<Long> frameBufferSize = parser
+                .accepts("framebuf", "Size of profiler framebuffer")
+                .withRequiredArg()
+                .ofType(Long.class);
+        OptionSpec<Long> interval = parser
+                .accepts("interval", "Profiling interval, in nanoseconds")
+                .withRequiredArg()
+                .ofType(Long.class);
+        OptionSpec<Boolean> threads = parser
+                .accepts("threads", "Profile threads separately")
+                .withRequiredArg()
+                .ofType(Boolean.class)
+                .defaultsTo(false, true);
+
+
+        OptionSet options = parseInitLine(initLine, parser);
+        if (options.has(event)) {
+            this.event = options.valueOf(event);
+        } else {
+            this.event = DEFAULT_EVENT;
+        }
+        if (options.has(frameBufferSize)) {
+            this.frameBufferSize = OptionalLong.of(options.valueOf(frameBufferSize));
+        } else {
+            this.frameBufferSize = OptionalLong.empty();
+        }
+        if (options.has(interval)) {
+            this.interval = OptionalLong.of(options.valueOf(interval));
+        } else {
+            this.interval = OptionalLong.empty();
+        }
+        if (options.has(outputDir)) {
+            this.outputDir = Paths.get(options.valueOf(outputDir));
+            createOutputDirectories();
+        }
+        if (options.has(threads)) {
+            this.threads = options.valueOf(threads);
+        } else {
+            this.threads = false;
+        }
+        if (options.has(asyncProfiler)) {
+            this.asyncProfilerDir = Paths.get(options.valueOf(asyncProfiler));
+        } else {
+            this.asyncProfilerDir = Objects.requireNonNull(asyncDir, "directory of async-profiler missing");
+        }
+    }
+
+    @Override
+    public Collection<String> addJVMInvokeOptions(final BenchmarkParams params) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Collection<String> addJVMOptions(final BenchmarkParams params) {
+        return Arrays.asList(
+                "-XX:+UnlockDiagnosticVMOptions",
+                "-XX:+DebugNonSafepoints"
+        );
+    }
+
+    @Override
+    public String getDescription() {
+        return "Generate flame graphs using async-profiler";
+    }
+
+    @Override
+    public void beforeTrial(final BenchmarkParams benchmarkParams) {
+    }
+
+    @Override
+    public Collection<? extends Result> afterTrial(
+            final BenchmarkResult br,
+            final long pid,
+            final File stdOut,
+            final File stdErr) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean allowPrintOut() {
+        return true;
+    }
+
+    @Override
+    public boolean allowPrintErr() {
+        return true;
+    }
+
+    @Override
+    public void beforeIteration(final BenchmarkParams benchmarkParams, final IterationParams iterationParams) {
+        if (iterationParams.getType() != IterationType.MEASUREMENT || started) {
+            return;
+        }
+
+        if (outputDir == null) {
+            outputDir = createTempDir(benchmarkParams);
+        }
+
+        Collection<String> commands = new ArrayList<>();
+        commands.add("-e");
+        commands.add(event);
+        if (threads) {
+            commands.add("-t");
+        }
+        frameBufferSize.ifPresent(b -> {
+            commands.add("-b");
+            commands.add(String.valueOf(b));
+        });
+        interval.ifPresent(i -> {
+            commands.add("-i");
+            commands.add(String.valueOf(i));
+        });
+        commands.add("-f");
+        commands.add(outputFile(benchmarkParams).toString());
+
+        profilerCommand("start", commands);
+        started = true;
+    }
+
+    @Override
+    public Collection<? extends Result> afterIteration(
+            final BenchmarkParams benchmarkParams,
+            final IterationParams iterationParams,
+            final IterationResult result) {
+        if (iterationParams.getType() != IterationType.MEASUREMENT) {
+            return Collections.emptyList();
+        }
+
+        if (++measurementIterationCount == iterationParams.getCount()) {
+            Path profileFile = outputFile(benchmarkParams);
+            profilerCommand("stop", "-f", profileFile.toString());
+            generated.add(profileFile);
+        }
+
+        if (generated.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return generated
+                .stream().map(Path::toAbsolutePath)
+                .map(Path::toString)
+                .map(f -> new NoResult("flame-graph", f))
+                .collect(Collectors.toList());
+    }
+
+    private Path createTempDir(final BenchmarkParams benchmarkParams) {
+        String benchmark = benchmarkParams.getBenchmark();
+        int i = benchmark.lastIndexOf('.');
+        if (i > 0) {
+            benchmark = benchmark.substring(0, i);
+        }
+        try {
+            return LdbcDownloader.tempDirFor("org.neo4j", "bench", benchmark);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final Pattern SLASH = Pattern.compile("/");
+
+    private Path outputFile(final BenchmarkParams benchmarkParams) {
+        return outputDir.resolve(outputFileName(benchmarkParams)).toAbsolutePath();
+    }
+
+    private String outputFileName(final BenchmarkParams benchmarkParams) {
+        String benchmark = benchmarkParams.getBenchmark();
+        int i = benchmark.lastIndexOf('.');
+        StringBuilder sb = new StringBuilder("profile-");
+        if (i > 0) {
+            sb.append(benchmark, i + 1, benchmark.length()).append("-");
+        }
+        sb.append(event).append("-").append(benchmarkParams.getMode());
+        for (String key : benchmarkParams.getParamsKeys()) {
+            sb
+                    .append("-")
+                    .append(key)
+                    .append("-")
+                    .append(benchmarkParams.getParam(key));
+        }
+        sb.append(".svg");
+        return SLASH.matcher(sb).replaceAll("-");
+    }
+
+    private void profilerCommand(final String action, final String... options) {
+        profilerCommand(action, Arrays.asList(options));
+    }
+
+    private void profilerCommand(final String action, final Collection<String> options) {
+        long pid = Utils.getPid();
+
+        List<String> cmd = new ArrayList<>();
+        cmd.add("./profiler.sh");
+        cmd.add(action);
+        cmd.addAll(options);
+        cmd.add(String.valueOf(pid));
+
+        ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+        processBuilder.directory(asyncProfilerDir.toFile());
+        startAndWait(processBuilder);
+    }
+
+    private void createOutputDirectories() {
+        createDirectories(outputDir);
+    }
+
+    private static OptionSet parseInitLine(final String initLine, final OptionParser parser) throws ProfilerException {
+        try {
+            Method method = Class
+                    .forName("org.openjdk.jmh.profile.ProfilerUtils")
+                    .getDeclaredMethod("parseInitLine", String.class, OptionParser.class);
+            method.setAccessible(true);
+            return (OptionSet) method.invoke(null, initLine, parser);
+        } catch (InvocationTargetException ex) {
+            throw (ProfilerException) ex.getCause();
+        } catch (NoSuchMethodException | IllegalArgumentException | IllegalAccessException | ClassNotFoundException | SecurityException e) {
+            throw new ProfilerException(e);
+        }
+    }
+
+    private static void startAndWait(final ProcessBuilder processBuilder) {
+        String commandLine = String.join(" ", processBuilder.command());
+        try {
+            processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
+            Process process = processBuilder.start();
+            if (process.waitFor() != 0) {
+                throw new RuntimeException("Non zero exit code from: " + commandLine);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Error running " + commandLine, e);
+        }
+    }
+
+    private static void createDirectories(final Path outputDir) {
+        try {
+            Files.createDirectories(outputDir);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
