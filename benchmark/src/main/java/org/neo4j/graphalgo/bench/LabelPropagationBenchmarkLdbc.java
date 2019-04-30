32c652f04f0769883ecfa16894590500e0393161
Paul Horn <phorn@avantgarde-labs.de>
2019-04-30 14:48:29 +0200

    Adjust LPA/LDBC benchmark

--- benchmark/src/main/java/org/neo4j/graphalgo/bench/LabelPropagationBenchmarkLdbc.java
+++ benchmark/src/main/java/org/neo4j/graphalgo/bench/LabelPropagationBenchmarkLdbc.java
@@ -18,37 +18,20 @@
  */
 package org.neo4j.graphalgo.bench;
 
-import org.neo4j.graphalgo.LabelPropagationProc;
 import org.neo4j.graphalgo.PropertyMapping;
 import org.neo4j.graphalgo.core.GraphLoader;
 import org.neo4j.graphalgo.core.heavyweight.HeavyGraph;
 import org.neo4j.graphalgo.core.heavyweight.HeavyGraphFactory;
+import org.neo4j.graphalgo.core.utils.ParallelUtil;
 import org.neo4j.graphalgo.core.utils.Pools;
 import org.neo4j.graphalgo.helper.ldbc.LdbcDownloader;
+import org.neo4j.graphalgo.impl.LabelPropagation;
 import org.neo4j.graphdb.Direction;
-import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.internal.kernel.api.exceptions.KernelException;
-import org.neo4j.kernel.impl.proc.Procedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 import static org.neo4j.graphalgo.impl.LabelPropagationAlgorithm.PARTITION_TYPE;
 import static org.neo4j.graphalgo.impl.LabelPropagationAlgorithm.WEIGHT_TYPE;
@@ -57,31 +40,23 @@ import static org.neo4j.graphalgo.impl.LabelPropagationAlgorithm.WEIGHT_TYPE;
  * @author mknobloch
  */
 @Threads(1)
-@Fork(1)
-@Warmup(iterations = 1, time = 1)
-@Measurement(iterations = 1, time = 1)
+@Fork(value = 1, jvmArgs = {"-Xms8g", "-Xmx8g", "-XX:+UseG1GC"})
+@Warmup(iterations = 2, time = 10)
+@Measurement(iterations = 4, time = 10)
 @State(Scope.Benchmark)
-@BenchmarkMode(Mode.SingleShotTime)
+@BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class LabelPropagationBenchmarkLdbc {
 
-    @Param({"1", "5"})
+    @Param({"1", "15"})
     int iterations;
 
-    @Param({"10000", "100000000"})
-    int batchSize;
-
-    private HeavyGraph graph;
     private GraphDatabaseAPI db;
-    private Transaction tx;
+    private HeavyGraph graph;
 
     @Setup
-    public void setup() throws KernelException, IOException {
+    public void setup() throws IOException {
         db = LdbcDownloader.openDb();
-
-        Procedures procedures = db.getDependencyResolver().resolveDependency(Procedures.class);
-        procedures.registerProcedure(LabelPropagationProc.class);
-
         graph = (HeavyGraph) new GraphLoader(db)
                 .withAnyLabel()
                 .withAnyRelationshipType()
@@ -94,67 +69,16 @@ public class LabelPropagationBenchmarkLdbc {
                 .load(HeavyGraphFactory.class);
     }
 
-    @Setup(Level.Invocation)
-    public void startTx() {
-        tx = db.beginTx();
-    }
-
     @TearDown
     public void shutdown() {
-        graph.release();
         db.shutdown();
         Pools.DEFAULT.shutdownNow();
     }
 
-    @TearDown(Level.Invocation)
-    public void failTx() {
-        tx.failure();
-        tx.close();
-    }
-
     @Benchmark
-    public Object _01_algo() {
-        return runPrintQuery(
-                db,
-                "CALL algo.labelPropagation(null, null, 'OUTGOING',{iterations:" + iterations + ",batchSize:" + batchSize + "}) "
-                        + "YIELD loadMillis, computeMillis, writeMillis"
-        );
-    }
-
-    @Benchmark
-    public Object _03_direct() {
+    public LabelPropagation lpa() {
         return new org.neo4j.graphalgo.impl
-                .LabelPropagation(graph, graph, batchSize, Pools.DEFAULT_CONCURRENCY, Pools.DEFAULT)
+                .LabelPropagation(graph, graph, ParallelUtil.DEFAULT_BATCH_SIZE, Pools.DEFAULT_CONCURRENCY, Pools.DEFAULT)
                 .compute(Direction.OUTGOING, iterations);
-    }
-
-    private static Object runPrintQuery(GraphDatabaseAPI db, String query) {
-        return runQuery(db, query, result -> {
-            long load = result.getNumber("loadMillis").longValue();
-            long compute = result.getNumber("computeMillis").longValue();
-            long write = result.getNumber("writeMillis").longValue();
-            System.out.printf(
-                    "load: %d ms%ncompute: %d ms%nwrite: %d ms%n",
-                    load,
-                    compute,
-                    write);
-        });
-    }
-
-    private static Object runVoidQuery(GraphDatabaseAPI db, String query) {
-        return runQuery(db, query, r -> {});
-    }
-
-    private static Object runQuery(
-            GraphDatabaseAPI db,
-            String query,
-            Consumer<Result.ResultRow> action) {
-        try (Result result = db.execute(query)) {
-            result.accept(r -> {
-                action.accept(r);
-                return true;
-            });
-        }
-        return db;
     }
 }
